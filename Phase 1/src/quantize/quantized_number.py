f19c5a962e88760403165638ef8ae7256ea73f65
Wenyang Zhou <zhouwenyang@outlook.com>
2023-12-12 10:57:48 +0800

    matrix multiplication

--- Phase 1/src/quantize/quantized_number.py
+++ Phase 1/src/quantize/quantized_number.py
@@ -2,32 +2,57 @@ import numpy as np
 
 
 class QuantizedArray:
-    def __init__(self, array, /, scale, zero_point, bit_width):
-        assert isinstance(bit_width, int), 'bit_width must be an integer'
-        assert 0 < bit_width <= 64, 'bit_width must be between 1 and 64'
-        self.bit_width = bit_width
-
+    def __init__(self, array, scale, zero_point, bit_width):
+        self.array = np.array(array, dtype=np.int64)
         self.scale = np.array(scale, dtype=np.float64)
         self.zero_point = np.array(zero_point, dtype=np.int64)
+        self.bit_width = np.array(bit_width, dtype=np.int32)
+
+    def __repr__(self):
+        return f'QuantizedArray({self.array}, scale={self.scale}, zero_point={self.zero_point}, bit_width={self.bit_width})'
+
+    @property
+    def shape(self):
+        return self.array.shape
+
+    @classmethod
+    def quantize(cls, array, scale, zero_point, bit_width):
+        assert isinstance(bit_width, int), 'bit_width must be an integer'
+        assert 0 < bit_width <= 64, 'bit_width must be between 1 and 64'
 
         array = np.array(array, dtype=np.float64) / scale + zero_point
         array = np.round(array)
         array = np.clip(array, -2 ** (bit_width - 1), 2 ** (bit_width - 1) - 1)
         array = array.astype(np.int64)
 
-        self.array = array
-
-    def __repr__(self):
-        return f'QuantizedArray({self.array}, scale={self.scale}, zero_point={self.zero_point}, bit_width={self.bit_width})'
+        return cls(array, scale, zero_point, bit_width)
 
     def dequantize(self):  # returns float64
         return (self.array - self.zero_point) * self.scale
 
     def add(self, other, scale, zero_point, bit_width):
         assert isinstance(other, QuantizedArray), 'Can only add QuantizedArrays'
-        assert self.bit_width == other.bit_width, 'Bit widths must be equal'
-        assert self.array.size == other.array.size, 'Arrays must be of equal size'
+        assert self.shape == other.shape, 'Arrays must be of equal shape'
 
         r = self.dequantize() + other.dequantize()
-        return QuantizedArray(r, scale, zero_point, bit_width)
+        return QuantizedArray.quantize(r, scale, zero_point, bit_width)
+
+    def matmul(self, other, scale, zero_point, result_bit_width, intermediate_bit_width=32):
+        assert isinstance(other, QuantizedArray), 'Can only add QuantizedArrays'
+        assert self.array.ndim == 2 and other.array.ndim == 2, 'Arrays must be 2D'
+        assert self.shape[1] == other.shape[0], 'Arrays must be of appropriate shape'
+
+        result = np.empty([self.shape[0], other.shape[1]], dtype=np.int64)
+
+        m = self.scale * other.scale / scale
+
+        for i in range(self.shape[0]):
+            for j in range(other.shape[1]):
+                q = np.zeros([1], dtype=np.int64)
+                for k in range(self.shape[1]):
+                    q += (self.array[i, k] - self.zero_point) * (other.array[k, j] - other.zero_point)
+                    q = np.clip(q, -2 ** (intermediate_bit_width - 1), 2 ** (intermediate_bit_width - 1) - 1)
+                q = np.round(q * m + zero_point)
+                result[i, j] = q
+        return QuantizedArray(result, scale, zero_point, result_bit_width)
 
--- Phase 1/src/quantized_number.py
+++ Phase 1/src/quantized_number.py
@@ -7,46 +7,110 @@ class TestQuantizedArray(unittest.TestCase):
 
     def test_creation(self):
         # Test basic creation of a QuantizedArray
-        qa = QuantizedArray([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=64)
+        qa = QuantizedArray.quantize([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=64)
         self.assertTrue(np.array_equal(qa.array, np.array([1, 2, 3])* 2**10))
 
     def test_bit_width_acc_loss(self):
-        qa = QuantizedArray([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=10)
+        qa = QuantizedArray.quantize([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=10)
         self.assertTrue(np.array_equal(qa.array, np.array([511, 511, 511])))
 
-        qa = QuantizedArray([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=11)
+        qa = QuantizedArray.quantize([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=11)
         self.assertTrue(np.array_equal(qa.array, np.full([3], 1023)))
 
-        qa = QuantizedArray([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=12)
+        qa = QuantizedArray.quantize([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=12)
         self.assertTrue(np.array_equal(qa.array, np.array([1024, 2047, 2047])))
 
 
     def test_bit_width(self):
         # Test if the bit_width is correctly set
-        qa = QuantizedArray([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=8)
+        qa = QuantizedArray.quantize([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=8)
         self.assertEqual(qa.bit_width, 8)
 
     def test_dequantize(self):
         arr = [1, 2, 3]
-        qa = QuantizedArray(arr, scale=2 ** -10, zero_point=0, bit_width=64)
+        qa = QuantizedArray.quantize(arr, scale=2 ** -10, zero_point=0, bit_width=64)
         self.assertTrue(np.allclose(qa.dequantize(), arr))
 
         arr = [1, 5, 9, 5]
-        qa = QuantizedArray(arr, scale=2 ** -10, zero_point=0, bit_width=64)
+        qa = QuantizedArray.quantize(arr, scale=2 ** -10, zero_point=0, bit_width=64)
         self.assertTrue(np.allclose(qa.dequantize(), arr))
 
         arr = [-2, -5, -7, 10]
-        qa = QuantizedArray(arr, scale=2 ** -10, zero_point=2**30, bit_width=64)
+        qa = QuantizedArray.quantize(arr, scale=2 ** -10, zero_point=2**30, bit_width=64)
         self.assertTrue(np.allclose(qa.dequantize(), arr))
 
     def test_add(self):
         arr1 = np.array([1, 2, 3])
         arr2 = np.array([3, 4, 5])
-        a = QuantizedArray(arr1, scale=2 ** -10, zero_point=2**30, bit_width=64)
-        b = QuantizedArray(arr2, scale=2 ** -10, zero_point=2**30, bit_width=64)
+        a = QuantizedArray.quantize(arr1, scale=2 ** -10, zero_point=2**30, bit_width=64)
+        b = QuantizedArray.quantize(arr2, scale=2 ** -10, zero_point=2**30, bit_width=64)
         c = a.add(b, scale=2 ** -10, zero_point=2**30, bit_width=64)
         self.assertTrue(np.allclose(c.dequantize(), arr1+arr2))
 
+class TestQuantizedArrayMatMul(unittest.TestCase):
+
+    def test_matmul_basic(self):
+        # Test basic matrix multiplication
+        arr1 = np.array([[1, 2], [3, 4]])
+        arr2 = np.array([[5, 6], [7, 8]])
+        scale = 2**-10
+        zero_point = 0
+        bit_width = 64
+
+        qa1 = QuantizedArray.quantize(arr1, scale, zero_point, bit_width)
+        qa2 = QuantizedArray.quantize(arr2, scale, zero_point, bit_width)
+
+        result = qa1.matmul(qa2, scale, zero_point, bit_width)
+        expected = np.matmul(arr1, arr2)
+
+        self.assertTrue(np.allclose(result.dequantize(), expected))
+
+    def test_matmul_identity(self):
+        # Test multiplication with an identity matrix
+        arr = np.array([[1, 2], [3, 4]])
+        identity = np.array([[1, 0], [0, 1]])
+        scale = 2 ** -10
+        zero_point = 0
+        bit_width = 64
+
+        qa = QuantizedArray.quantize(arr, scale, zero_point, bit_width)
+        qa_identity = QuantizedArray.quantize(identity, scale, zero_point, bit_width)
+
+        result = qa.matmul(qa_identity, scale, zero_point, bit_width)
+
+        self.assertTrue(np.allclose(result.dequantize(), arr))
+
+    def test_matmul_zero_matrix(self):
+        # Test multiplication with a zero matrix
+        arr = np.array([[1, 2], [3, 4]])
+        zero_matrix = np.array([[0, 0], [0, 0]])
+        scale = 2 ** -10
+        zero_point = 0
+        bit_width = 64
+
+        qa = QuantizedArray.quantize(arr, scale, zero_point, bit_width)
+        qa_zero = QuantizedArray.quantize(zero_matrix, scale, zero_point, bit_width)
+
+        result = qa.matmul(qa_zero, scale, zero_point, bit_width)
+
+        self.assertTrue(np.allclose(result.dequantize(), zero_matrix))
+
+    def test_matmul_different_bit_width(self):
+        # Test matrix multiplication with different bit widths
+        arr1 = np.array([[1, 2], [3, 4]])
+        arr2 = np.array([[5, 6], [7, 8]])
+        scale = 2 ** -10
+        zero_point = 0
+        bit_width_1 = 64
+        bit_width_2 = 32
+
+        qa1 = QuantizedArray.quantize(arr1, scale, zero_point, bit_width_1)
+        qa2 = QuantizedArray.quantize(arr2, scale, zero_point, bit_width_2)
+
+        result = qa1.matmul(qa2, scale, zero_point, bit_width_1, intermediate_bit_width=bit_width_2)
+        expected = np.matmul(arr1, arr2)
+
+        self.assertTrue(np.allclose(result.dequantize(), expected))
 
 
 
