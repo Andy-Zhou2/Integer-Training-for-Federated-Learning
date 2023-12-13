0320a62f54673a1be16547224dbc1df3c353cd86
Wenyang Zhou <zhouwenyang@outlook.com>
2023-12-13 11:31:52 +0800

    per-channel quantized array

--- Phase 1/src/quantize/quantized_number.py
+++ Phase 1/src/quantize/quantized_number.py
@@ -1,12 +1,23 @@
 import numpy as np
+from typing import Union
+
+
+def quantize_and_clip(array, scale, zero_point, bit_width):
+    array = np.array(array, dtype=np.float64) / scale + zero_point
+    array = np.round(array)
+    array = np.clip(array, -2 ** (bit_width - 1), 2 ** (bit_width - 1) - 1)
+    array = array.astype(np.int64)
+
+    return array
 
 
 class QuantizedArray:
-    def __init__(self, array, scale, zero_point, bit_width):
+    def __init__(self, array, scale, zero_point, bit_width, per_channel=False):
         self.array = np.array(array, dtype=np.int64)
         self.scale = np.array(scale, dtype=np.float64)
         self.zero_point = np.array(zero_point, dtype=np.int64)
         self.bit_width = np.array(bit_width, dtype=np.int32)
+        self.per_channel = per_channel
 
     def __repr__(self):
         return f'QuantizedArray({self.array}, scale={self.scale}, zero_point={self.zero_point}, bit_width={self.bit_width})'
@@ -16,16 +27,24 @@ class QuantizedArray:
         return self.array.shape
 
     @classmethod
-    def quantize(cls, array, scale, zero_point, bit_width):
-        assert isinstance(bit_width, int), 'bit_width must be an integer'
+    def quantize_per_tensor(cls, array: Union[list, np.ndarray], scale: Union[float, int], zero_point: int,
+                            bit_width: int):
         assert 0 < bit_width <= 64, 'bit_width must be between 1 and 64'
 
-        array = np.array(array, dtype=np.float64) / scale + zero_point
-        array = np.round(array)
-        array = np.clip(array, -2 ** (bit_width - 1), 2 ** (bit_width - 1) - 1)
-        array = array.astype(np.int64)
+        array = quantize_and_clip(array, scale, zero_point, bit_width)
 
         return cls(array, scale, zero_point, bit_width)
+
+    @classmethod
+    def quantize_per_channel(cls, array: np.ndarray, scale: np.ndarray, zero_point: np.ndarray, bit_width: int):
+        assert 0 < bit_width <= 64, 'bit_width must be between 1 and 64'
+        assert array.shape[0] == scale.shape[0] == zero_point.shape[0], \
+            'array, scale, and zero_point must have the same number of channels'
+        result = np.empty(array.shape, dtype=np.int64)
+        for ch in range(len(scale)):
+            result[ch] = quantize_and_clip(array[ch], scale[ch], zero_point[ch], bit_width)
+
+        return cls(result, scale, zero_point, bit_width, per_channel=True)
 
     def dequantize(self):  # returns float64
         return (self.array - self.zero_point) * self.scale
@@ -35,12 +54,13 @@ class QuantizedArray:
         assert self.shape == other.shape, 'Arrays must be of equal shape'
 
         r = self.dequantize() + other.dequantize()
-        return QuantizedArray.quantize(r, scale, zero_point, bit_width)
+        return QuantizedArray.quantize_per_tensor(r, scale, zero_point, bit_width)
 
     def matmul(self, other, scale, zero_point, result_bit_width, intermediate_bit_width=32):
         assert isinstance(other, QuantizedArray), 'Can only add QuantizedArrays'
         assert self.array.ndim == 2 and other.array.ndim == 2, 'Arrays must be 2D'
         assert self.shape[1] == other.shape[0], 'Arrays must be of appropriate shape'
+        assert not self.per_channel and not other.per_channel, 'Per-channel quantization not supported'
 
         result = np.empty([self.shape[0], other.shape[1]], dtype=np.int64)
 
@@ -55,4 +75,3 @@ class QuantizedArray:
                 q = np.round(q * m + zero_point)
                 result[i, j] = q
         return QuantizedArray(result, scale, zero_point, result_bit_width)
-
--- Phase 1/src/test_quantized_number.py
+++ Phase 1/src/test_quantized_number.py
@@ -3,49 +3,59 @@ import numpy as np
 from quantize.quantized_number import QuantizedArray
 
 
-class TestQuantizedArray(unittest.TestCase):
+class TestQuantizedArrayCreation(unittest.TestCase):
 
-    def test_creation(self):
-        # Test basic creation of a QuantizedArray
-        qa = QuantizedArray.quantize([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=64)
-        self.assertTrue(np.array_equal(qa.array, np.array([1, 2, 3])* 2**10))
+    def test_creation_per_tensor(self):
+        qa = QuantizedArray.quantize_per_tensor([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=64)
+        self.assertTrue(np.array_equal(qa.array, np.array([1, 2, 3]) * 2 ** 10))
+
+    def test_creation_per_channel(self):
+        qa = QuantizedArray.quantize_per_channel(np.array([1., 2.]), scale=np.array([1e-2, 1e-1]),
+                                                 zero_point=np.array([0, 1]), bit_width=64)
+        self.assertTrue(np.array_equal(qa.array, np.array([100, 21])))
 
     def test_bit_width_acc_loss(self):
-        qa = QuantizedArray.quantize([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=10)
+        qa = QuantizedArray.quantize_per_tensor([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=10)
         self.assertTrue(np.array_equal(qa.array, np.array([511, 511, 511])))
 
-        qa = QuantizedArray.quantize([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=11)
+        qa = QuantizedArray.quantize_per_tensor([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=11)
         self.assertTrue(np.array_equal(qa.array, np.full([3], 1023)))
 
-        qa = QuantizedArray.quantize([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=12)
+        qa = QuantizedArray.quantize_per_tensor([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=12)
         self.assertTrue(np.array_equal(qa.array, np.array([1024, 2047, 2047])))
-
 
     def test_bit_width(self):
         # Test if the bit_width is correctly set
-        qa = QuantizedArray.quantize([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=8)
+        qa = QuantizedArray.quantize_per_tensor([1, 2, 3], scale=2 ** -10, zero_point=0, bit_width=8)
         self.assertEqual(qa.bit_width, 8)
 
-    def test_dequantize(self):
+    def test_dequantize_per_tensor(self):
         arr = [1, 2, 3]
-        qa = QuantizedArray.quantize(arr, scale=2 ** -10, zero_point=0, bit_width=64)
+        qa = QuantizedArray.quantize_per_tensor(arr, scale=2 ** -10, zero_point=0, bit_width=64)
         self.assertTrue(np.allclose(qa.dequantize(), arr))
 
         arr = [1, 5, 9, 5]
-        qa = QuantizedArray.quantize(arr, scale=2 ** -10, zero_point=0, bit_width=64)
+        qa = QuantizedArray.quantize_per_tensor(arr, scale=2 ** -10, zero_point=0, bit_width=64)
         self.assertTrue(np.allclose(qa.dequantize(), arr))
 
         arr = [-2, -5, -7, 10]
-        qa = QuantizedArray.quantize(arr, scale=2 ** -10, zero_point=2**30, bit_width=64)
+        qa = QuantizedArray.quantize_per_tensor(arr, scale=2 ** -10, zero_point=2 ** 30, bit_width=64)
+        self.assertTrue(np.allclose(qa.dequantize(), arr))
+
+    def test_dequantize_per_channel(self):
+        arr = np.array([1., 2.])
+        qa = QuantizedArray.quantize_per_channel(arr, scale=np.array([1e-2, 1e-1]),
+                                                 zero_point=np.array([0, 1]), bit_width=64)
         self.assertTrue(np.allclose(qa.dequantize(), arr))
 
     def test_add(self):
         arr1 = np.array([1, 2, 3])
         arr2 = np.array([3, 4, 5])
-        a = QuantizedArray.quantize(arr1, scale=2 ** -10, zero_point=2**30, bit_width=64)
-        b = QuantizedArray.quantize(arr2, scale=2 ** -10, zero_point=2**30, bit_width=64)
-        c = a.add(b, scale=2 ** -10, zero_point=2**30, bit_width=64)
-        self.assertTrue(np.allclose(c.dequantize(), arr1+arr2))
+        a = QuantizedArray.quantize_per_tensor(arr1, scale=2 ** -10, zero_point=2 ** 30, bit_width=64)
+        b = QuantizedArray.quantize_per_tensor(arr2, scale=2 ** -10, zero_point=2 ** 30, bit_width=64)
+        c = a.add(b, scale=2 ** -10, zero_point=2 ** 30, bit_width=64)
+        self.assertTrue(np.allclose(c.dequantize(), arr1 + arr2))
+
 
 class TestQuantizedArrayMatMul(unittest.TestCase):
 
@@ -53,12 +63,12 @@ class TestQuantizedArrayMatMul(unittest.TestCase):
         # Test basic matrix multiplication
         arr1 = np.array([[1, 2], [3, 4]])
         arr2 = np.array([[5, 6], [7, 8]])
-        scale = 2**-10
+        scale = 2 ** -10
         zero_point = 0
         bit_width = 64
 
-        qa1 = QuantizedArray.quantize(arr1, scale, zero_point, bit_width)
-        qa2 = QuantizedArray.quantize(arr2, scale, zero_point, bit_width)
+        qa1 = QuantizedArray.quantize_per_tensor(arr1, scale, zero_point, bit_width)
+        qa2 = QuantizedArray.quantize_per_tensor(arr2, scale, zero_point, bit_width)
 
         result = qa1.matmul(qa2, scale, zero_point, bit_width)
         expected = np.matmul(arr1, arr2)
@@ -73,8 +83,8 @@ class TestQuantizedArrayMatMul(unittest.TestCase):
         zero_point = 0
         bit_width = 64
 
-        qa = QuantizedArray.quantize(arr, scale, zero_point, bit_width)
-        qa_identity = QuantizedArray.quantize(identity, scale, zero_point, bit_width)
+        qa = QuantizedArray.quantize_per_tensor(arr, scale, zero_point, bit_width)
+        qa_identity = QuantizedArray.quantize_per_tensor(identity, scale, zero_point, bit_width)
 
         result = qa.matmul(qa_identity, scale, zero_point, bit_width)
 
@@ -88,8 +98,8 @@ class TestQuantizedArrayMatMul(unittest.TestCase):
         zero_point = 0
         bit_width = 64
 
-        qa = QuantizedArray.quantize(arr, scale, zero_point, bit_width)
-        qa_zero = QuantizedArray.quantize(zero_matrix, scale, zero_point, bit_width)
+        qa = QuantizedArray.quantize_per_tensor(arr, scale, zero_point, bit_width)
+        qa_zero = QuantizedArray.quantize_per_tensor(zero_matrix, scale, zero_point, bit_width)
 
         result = qa.matmul(qa_zero, scale, zero_point, bit_width)
 
@@ -104,14 +114,13 @@ class TestQuantizedArrayMatMul(unittest.TestCase):
         bit_width_1 = 64
         bit_width_2 = 32
 
-        qa1 = QuantizedArray.quantize(arr1, scale, zero_point, bit_width_1)
-        qa2 = QuantizedArray.quantize(arr2, scale, zero_point, bit_width_2)
+        qa1 = QuantizedArray.quantize_per_tensor(arr1, scale, zero_point, bit_width_1)
+        qa2 = QuantizedArray.quantize_per_tensor(arr2, scale, zero_point, bit_width_2)
 
         result = qa1.matmul(qa2, scale, zero_point, bit_width_1, intermediate_bit_width=bit_width_2)
         expected = np.matmul(arr1, arr2)
 
         self.assertTrue(np.allclose(result.dequantize(), expected))
-
 
 
 if __name__ == '__main__':
