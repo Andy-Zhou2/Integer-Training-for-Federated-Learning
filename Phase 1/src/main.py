fix quantization overflow by clipping; fix bugs

Phase 1/src/main.py
@@ -29,7 +29,7 @@ def get_q_params_from_range(min_value: float, max_value: float, bit_width: int):
         max_value = 0
 
     scale = (max_value - min_value) / (2 ** bit_width - 1)
-    unsigned_zero_point = np.round(-min_value / scale).astype(np.int_)
+    unsigned_zero_point = np.round(-min_value / scale).astype(np.int64)
 
     if unsigned_zero_point == 0:
         scale = max_value / (2 ** bit_width - 1)
@@ -43,9 +43,10 @@ def get_q_params_from_range(min_value: float, max_value: float, bit_width: int):
     return scale, zero_point
 
 
-def get_per_channel_q_params(weight):
+def get_per_channel_q_params(weight, bit_width):
     """
     Calculates the per-channel scale and zero_point of the quantized weight (or bias).
+    :param bit_width: The bit-width used
     :param weight: The weight (or bias) to be quantized
     :return: (array of scales, array of zero_points)
     """
@@ -56,7 +57,7 @@ def get_per_channel_q_params(weight):
     for ch in range(num_channels):
         scales[ch], zero_points[ch] = get_q_params_from_range(weight[ch].min().item(),
                                                               weight[ch].max().item(),
-                                                              bit_width=8)
+                                                              bit_width=bit_width)
     return scales, zero_points
 
 
@@ -68,21 +69,25 @@ input_scale, input_zero_point = get_q_params_from_range(min_value=-0.42421296238
 data = QuantizedArray.quantize_per_tensor(data, input_scale, input_zero_point, input_bit_width)
 
 conv1_weight = model_ckpt['conv1.weight']
+# print(f'weight before quantization: {conv1_weight.numpy()}')
 conv1_weight_bit_width = 8
-conv1_weight_scale, conv1_weight_zero_point = get_per_channel_q_params(conv1_weight)
+conv1_weight_scale, conv1_weight_zero_point = get_per_channel_q_params(conv1_weight, conv1_weight_bit_width)
+# print(f'scale: {conv1_weight_scale}, zero_point: {conv1_weight_zero_point}')
 conv1_weight = QuantizedArray.quantize_per_channel(conv1_weight, conv1_weight_scale, conv1_weight_zero_point,
                                                    conv1_weight_bit_width)
 
+# print(f'weight after quantization: {conv1_weight.dequantize()}')
+
 conv1_bias = model_ckpt['conv1.bias']
-print(f'Maximum bias: {conv1_bias.max()}, Minimum bias: {conv1_bias.min()}')
+# print(f'Maximum bias: {conv1_bias.max()}, Minimum bias: {conv1_bias.min()}')
 conv1_bias_bit_width = 32
 conv1_bias_scale = input_scale * conv1_weight_scale
 conv1_bias_zero_point = np.zeros(conv1_bias.shape, dtype=np.int64)
 conv1_bias = QuantizedArray.quantize_per_channel(conv1_bias, conv1_bias_scale, conv1_bias_zero_point,
                                                  conv1_bias_bit_width)
 # sanity check
-print(f'Scale: {conv1_bias_scale}')
-print(f'Maximum value represented: {conv1_bias_scale * (2 ** (conv1_bias_bit_width - 1) - 1)}')
+# print(f'Scale: {conv1_bias_scale}')
+# print(f'Maximum value represented: {conv1_bias_scale * (2 ** (conv1_bias_bit_width - 1) - 1)}')
 
 act1_bit_width = 8
 act1_scale, act1_zero_point = get_q_params_from_range(min_value=0,  # simulate relu
@@ -92,5 +97,8 @@ act1_scale, act1_zero_point = get_q_params_from_range(min_value=0,  # simulate r
 act1_M0repr, act1_n = compute_M0repr_and_n(input_scale, conv1_weight_scale, act1_scale)
 
 act1 = data.conv2d(conv1_weight, conv1_bias, act1_scale, act1_zero_point, act1_bit_width, act1_M0repr, act1_n)
+act1_dq = act1.dequantize()
 print(act1.dequantize())
+print(f'Maximum of dq: {act1_dq.max()}, Minimum of dq: {act1_dq.min()}')
+print(f'value at max position: {act1_dq[3, 23, 5]}')
 

Phase 1/src/quantize/quantized_number.py
@@ -33,8 +33,12 @@ def compute_M0repr_and_n(input_scale, weights_scale, output_scale):
 class QuantizedArray:
     def __init__(self, array, scale, zero_point, bit_width, per_channel=False):
         # check if array is consistent with bit_width
+        assert 1 <= bit_width <= 32, 'bit_width must be between 1 and 32'
         assert np.all(array <= 2 ** (bit_width - 1) - 1) and np.all(array >= -2 ** (bit_width - 1)), \
             'array is not consistent with bit_width'
+        assert np.all(zero_point <= 2 ** (bit_width - 1) - 1) and np.all(zero_point >= -2 ** (bit_width - 1)), \
+            'zero_point is not consistent with bit_width'
+        assert np.all(scale > 0), 'scale must be positive'
 
         self.array = np.array(array, dtype=np.int64)
         self.scale = np.array(scale, dtype=np.float64)
@@ -52,7 +56,7 @@ class QuantizedArray:
     @classmethod
     def quantize_per_tensor(cls, array: Union[list, np.ndarray], scale: Union[float, int], zero_point: int,
                             bit_width: int):
-        assert 0 < bit_width <= 64, 'bit_width must be between 1 and 64'
+        assert 0 < bit_width <= 32, 'bit_width must be between 1 and 32'
 
         array = quantize_and_clip(array, scale, zero_point, bit_width)
 
@@ -60,7 +64,7 @@ class QuantizedArray:
 
     @classmethod
     def quantize_per_channel(cls, array: np.ndarray, scale: np.ndarray, zero_point: np.ndarray, bit_width: int):
-        assert 0 < bit_width <= 64, 'bit_width must be between 1 and 64'
+        assert 0 < bit_width <= 32, 'bit_width must be between 1 and 32'
         assert array.shape[0] == scale.shape[0] == zero_point.shape[0], \
             'array, scale, and zero_point must have the same number of channels'
         result = np.empty(array.shape, dtype=np.int64)
@@ -131,15 +135,29 @@ class QuantizedArray:
             for h in range(outH):
                 for w in range(outW):
                     # Convolution
-                    q = np.zeros([1], dtype=np.int32)
+                    q = np.zeros([1], dtype=np.int64)  # but values clipped within 32 bit
                     for c_in in range(C_in):
                         for kh in range(kH):
                             for kw in range(kW):
-                                q += (weight.array[c_out, c_in, kh, kw] - weight.zero_point[c_out]) * \
-                                     (self.array[c_in, h + kh, w + kw] - self.zero_point)
+                                a1 = np.clip(weight.array[c_out, c_in, kh, kw] - weight.zero_point[c_out], -2 ** 31,
+                                             2 ** 31 - 1)
+                                a2 = np.clip(self.array[c_in, h + kh, w + kw] - self.zero_point, -2 ** 31, 2 ** 31 - 1)
+                                delta = a1 * a2  # 2 ** 32 squared would overflow int64, so clip each term
+
+                                # clip delta, prevent overflow
+                                delta = np.clip(delta, np.iinfo(np.int32).min - q, np.iinfo(np.int32).max - q)
+                                q += delta
+                                # if delta > np.iinfo(np.int32).max - q:
+                                #     q = np.iinfo(np.int32).max
+                                # elif delta < np.iinfo(np.int32).min - q:
+                                #     q = np.iinfo(np.int32).min
+                                # else:
+                                #     q += delta
 
                     # Add bias
-                    q += bias.array[c_out]
+                    delta = bias.array[c_out]
+                    delta = np.clip(delta, np.iinfo(np.int32).min - q, np.iinfo(np.int32).max - q)
+                    q += delta
 
                     # multiply by M
                     q = np.multiply(q, M0_repr[c_out], dtype=np.int64)