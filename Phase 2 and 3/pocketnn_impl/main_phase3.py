9d11b7b5c0380ad58232b3f82d4462d87e48a337
Wenyang Zhou <zhouwenyang@outlook.com>
2024-04-17 08:32:09 +0100

    fix log main3.py

--- Phase 2 and 3/pocketnn_impl/main_phase3.py
+++ Phase 2 and 3/pocketnn_impl/main_phase3.py
@@ -18,8 +18,8 @@ def main(config: DictConfig):
     final_round_acc = centralized_acc[-1][1]
     max_acc = max([acc for _, acc in centralized_acc])
 
-    log(f'Final round accuracy: {final_round_acc}')
-    log(f'Max accuracy: {max_acc}')
+    log(logging.INFO, f'Final round accuracy: {final_round_acc}')
+    log(logging.INFO, f'Max accuracy: {max_acc}')
 
 
 if __name__ == '__main__':
