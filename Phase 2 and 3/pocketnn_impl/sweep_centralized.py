7cf76aebf1586d71edb93cd0d8c3f621ae9522ae
Andy <20140522+Andy-Zhou2@users.noreply.github.com>
2024-04-20 20:20:42 +0100

    sweep for centralized mnist bw

---
+++ Phase 2 and 3/pocketnn_impl/sweep_centralized.py
@@ -0,0 +1,71 @@
+import wandb
+import os
+from network import get_net
+from train_evaluate import pktnn_train, pktnn_evaluate
+from dataset import get_dataset
+import logging
+from utils_random import set_seed
+import shutil
+
+
+def agent_sweep():
+    with wandb.init():
+        # set logging level to info
+        logging.basicConfig(level=logging.INFO)
+        config = wandb.config
+
+        dataset_name = config.dataset
+        assert dataset_name in ['mnist', 'fashion_mnist']
+
+        train_data, test_data = get_dataset(dataset_name)
+
+        layer1_clip = 2 ** (config.layer_1_bw - 1) - 1
+        layer2_clip = 2 ** (config.layer_2_bw - 1) - 1
+        layer3_clip = 2 ** (config.layer_3_bw - 1) - 1
+        model_name = f'mnist_default [{layer1_clip},{layer2_clip},{layer3_clip}]'
+
+        custom_name = f'cent_lr{config.initial_lr_inv}_bs{config.batch_size}_model{model_name}'
+        wandb.run.name = custom_name
+        # create folder
+        weight_folder = ''  # don't save for sweep
+
+        logging.info(f'Creating model: {model_name}')
+        net = get_net(model_name)
+
+        seed = config.seed
+        set_seed(seed)
+
+        config = {
+            'epochs': config.epochs,
+            'batch_size': config.batch_size,
+            'initial_lr_inv': config.initial_lr_inv,
+            'weight_folder': weight_folder,
+            'test_every_epoch': config.test_every_epoch,
+            'print_hash_every_epoch': config.print_hash_every_epoch,
+            'shuffle_dataset_every_epoch': config.shuffle_dataset_every_epoch,
+            'verbose': config.verbose,
+            'label_target_value': config.label_target_value,
+        }
+
+        pkt_data = {
+            'train': train_data,
+            'test': test_data
+        }
+        result = pktnn_train(net, pkt_data, config)
+        logging.info(f'Train completed. Result: ')
+        logging.info(result)
+
+        for epoch in range(config['epochs']):
+            wandb.log({'loss': result['loss'][epoch]})
+            wandb.log({'train_accuracy': result['train_accuracy'][epoch]})
+            if result['test_accuracy']:  # not empty
+                wandb.log({'test_accuracy': result['test_accuracy'][epoch]})
+
+        max_acc = max(result['test_accuracy'])
+
+        wandb.log({'max_acc': max_acc})
+
+
+if __name__ == '__main__':
+    wandb.agent(sweep_id='xz2txrta', function=agent_sweep,
+                project='part ii diss', entity='wz337', count=100)
---
+++ Phase 2 and 3/pocketnn_impl/sweep_init_centralized.py
@@ -0,0 +1,72 @@
+import wandb
+import pprint
+
+sweep_config = {
+    'method': 'random',
+    'metric': {
+        'name': 'max_acc',
+        'goal': 'maximize'
+    },
+}
+
+parameters_dict = {
+    'initial_lr_inv': {
+        'distribution': 'int_uniform',
+        'min': 100,
+        'max': 3000
+    },
+    'batch_size': {
+        'distribution': 'int_uniform',
+        'min': 1,
+        'max': 40
+    },
+    'layer_1_bw': {  # using mnist_default
+        'distribution': 'int_uniform',
+        'min': 1,
+        'max': 16
+    },
+    'layer_2_bw': {
+        'distribution': 'int_uniform',
+        'min': 1,
+        'max': 16
+    },
+    'layer_3_bw': {
+        'distribution': 'int_uniform',
+        'min': 1,
+        'max': 16
+    },
+    'label_target_value': {
+        'values': [1, 3, 7, 15, 31, 63, 127, 255]
+    }
+}
+
+# also set fixed parameters
+parameters_dict.update({
+    'shuffle_dataset_every_epoch': {
+        'value': True
+    },
+    'dataset': {
+        'value': 'fashion_mnist'
+    },
+    'test_every_epoch': {
+        'value': True
+    },
+    'seed': {
+        'value': 123
+    },
+    'verbose': {
+        'value': True
+    },
+    'epochs': {
+        'value': 50
+    },
+    'print_hash_every_epoch': {
+        'value': False
+    },
+})
+
+sweep_config['parameters'] = parameters_dict
+
+pprint.pprint(sweep_config)
+
+sweep_id = wandb.sweep(sweep_config, project='part ii diss', entity='wz337')
