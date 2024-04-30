6d2a967198aee9ae5ff5f91fb4c5693f4debb800
Wenyang Zhou <zhouwenyang@outlook.com>
2024-04-30 11:39:56 +0100

    save dfa weight as well

--- Phase 2 and 3/src/pktnn/pkt_state.py
+++ Phase 2 and 3/src/pktnn/pkt_state.py
@@ -8,11 +8,13 @@ def save_state(fcs: List[PktFc], filename: str):
     for fc in fcs:
         weights.append(fc.weight.mat)
         weights.append(fc.bias.mat)
+        weights.append(fc.dfa_weight.mat)
     np.savez(filename, *weights)
 
 
 def load_state(fcs: List[PktFc], filename: str):
     weights = np.load(filename, allow_pickle=True)
     for i in range(len(fcs)):
-        fcs[i].weight.mat = weights['arr_%d' % (i * 2)]
-        fcs[i].bias.mat = weights['arr_%d' % (i * 2 + 1)]
+        fcs[i].weight.mat = weights['arr_%d' % (i * 3)]
+        fcs[i].bias.mat = weights['arr_%d' % (i * 3 + 1)]
+        fcs[i].dfa_weight.mat = weights['arr_%d' % (i * 3 + 2)]
