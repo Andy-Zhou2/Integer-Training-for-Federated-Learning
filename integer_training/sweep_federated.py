459ae3903fc1994d9de0ce68d6730fb076477635
Wenyang Zhou <zhouwenyang@outlook.com>
2024-05-08 22:35:50 +0100

    sweep for fed: grid search for bw

--- integer_training/sweep_federated.py
+++ integer_training/sweep_federated.py
@@ -4,7 +4,7 @@ from src.fl.pkt_fl import simulate
 
 def agent_sweep():
     with wandb.init():
-        custom_name = f'p{wandb.config.fraction_fit}_lr{wandb.config.lr_inv}_bs{wandb.config.batch_size}_ep{wandb.config.epochs}_alpha{wandb.config.dataset_dirichlet_alpha}_model{wandb.config.model_name}'
+        custom_name = f'model{wandb.config.model_name}'
         wandb.run.name = custom_name
 
         config = wandb.config
@@ -18,5 +18,5 @@ def agent_sweep():
 
 
 if __name__ == '__main__':
-    wandb.agent(sweep_id='s2lg6h0d', function=agent_sweep,
+    wandb.agent(sweep_id='exmj06q2', function=agent_sweep,
                 project='part ii diss', entity='wz337', count=100)
--- integer_training/sweep_init/sweep_init_federated.py
+++ integer_training/sweep_init/sweep_init_federated.py
@@ -2,47 +2,45 @@ import wandb
 import pprint
 
 sweep_config = {
-    'method': 'random',
+    'method': 'grid',
     'metric': {
         'name': 'max_acc',
         'goal': 'maximize'
     },
 }
 
+model_names = []
+for bw1 in [4, 6, 8, 10, 12, 16, 20]:
+    for bw2 in [4, 6, 8, 10, 12, 16, 20]:
+        for bw3 in [4, 6, 8, 10, 12, 16, 20]:
+            cap1 = 2 ** (bw1 - 1) - 1
+            cap2 = 2 ** (bw2 - 1) - 1
+            cap3 = 2 ** (bw3 - 1) - 1
+            model_names.append(f'mnist_default [{cap1},{cap2},{cap3}]')
+
 parameters_dict = {
-    'lr_inv': {
-        'distribution': 'int_uniform',
-        'min': 100,
-        'max': 3000
-    },
-    'batch_size': {
-        'distribution': 'int_uniform',
-        'min': 10,
-        'max': 40
-    },
-    'epochs': {
-        'distribution': 'int_uniform',
-        'min': 1,
-        'max': 20
-    },
-    'dataset_dirichlet_alpha': {
-        'values': [0.1, 1, 1000]
-    },
     'model_name': {
-        'values': ['custom []',
-                   'custom [50]', 'custom [100]', 'custom [200]', 'custom [100, 100]',
-                   'custom [200, 100]', 'custom [200, 100, 50]', 'custom [400, 200, 100]',
-                   'custom [400, 200, 100, 50]',
-                   'custom [400, 200, 200, 100, 50]',
-                   'fashion_mnist_default', 'mnist_default']
+        'values': model_names
     },
-    'fraction_fit': {
-        'values': [0.15, 1]
-    }
 }
 
 # also set fixed parameters
 parameters_dict.update({
+    'dataset_dirichlet_alpha': {
+        'value': 1
+    },
+    'fraction_fit': {
+        'value': 0.15
+    },
+    'epochs': {
+        'value': 1
+    },
+    'batch_size': {
+        'value': 20
+    },
+    'lr_inv': {
+        'value': 1000
+    },
     'shuffle_dataset_every_epoch': {
         'value': True
     },
@@ -50,10 +48,10 @@ parameters_dict.update({
         'value': 100
     },
     'dataset_name': {
-        'value': 'fashion_mnist'
+        'value': 'mnist'
     },
     'num_rounds': {
-        'value': 40
+        'value': 500
     },
     'client_resources': {
         'value': {"num_cpus": 1, "num_gpus": 0.0}
@@ -75,7 +73,16 @@ parameters_dict.update({
     },
     'train_verbose': {
         'value': False
-    }
+    },
+    'label_target_value': {
+        'value': 15
+    },
+    'gamma_inv': {
+        'value': 2
+    },
+    'gamma_step': {
+        'value': 10
+    },
 })
 
 sweep_config['parameters'] = parameters_dict
